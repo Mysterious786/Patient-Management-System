7be465c2d9ab4723d2c5b8ae5272b0cdfa97f7fd
Saqlain Ansari <saqlainansari@Saqlains-MacBook-Air.local>
2025-10-14 11:34:20 +0530

    feat: added email exist validation

--- src/main/java/com/example/patientservice/service/PatientService.java
+++ src/main/java/com/example/patientservice/service/PatientService.java
@@ -3,6 +3,7 @@ package com.example.patientservice.service;
 
 import com.example.patientservice.dto.PatientRequestDTO;
 import com.example.patientservice.dto.PatientResponseDTO;
+import com.example.patientservice.exception.EmailAlreadyExistsException;
 import com.example.patientservice.mapper.PatientMapper;
 import com.example.patientservice.model.Patient;
 import com.example.patientservice.repository.PatientRepository;
@@ -28,6 +29,10 @@ public class PatientService {
     public PatientResponseDTO createPatient(PatientRequestDTO patientRequestDTO)
     {
         Patient newPatient = patientRepository.save(PatientMapper.toModel(patientRequestDTO));
+        if (patientRepository.existsByEmail(patientRequestDTO.getEmail()))
+        {
+            throw new EmailAlreadyExistsException("A patient with this email already exists!" + patientRequestDTO.getEmail());
+        }
         return PatientMapper.toDTO(newPatient);
     }
 
