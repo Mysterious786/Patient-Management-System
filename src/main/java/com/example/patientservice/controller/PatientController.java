1c5c36b8cf85938ee17bd83141deb5bed845ad92
Saqlain Ansari <saqlainansari@Saqlains-MacBook-Air.local>
2025-10-17 21:24:16 +0530

    feat: add validation for put method, create createPatientValidation for custom classes validation

--- src/main/java/com/example/patientservice/controller/PatientController.java
+++ src/main/java/com/example/patientservice/controller/PatientController.java
@@ -3,10 +3,14 @@ package com.example.patientservice.controller;
 
 import com.example.patientservice.dto.PatientRequestDTO;
 import com.example.patientservice.dto.PatientResponseDTO;
+import com.example.patientservice.dto.validators.CreatePatientValidationGroup;
 import com.example.patientservice.model.Patient;
 import com.example.patientservice.service.PatientService;
+import com.fasterxml.jackson.databind.ser.std.StdKeySerializers;
 import jakarta.validation.Valid;
+import jakarta.validation.groups.Default;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -32,7 +36,7 @@ public class PatientController {
     }
 
     @PostMapping
-    public ResponseEntity<PatientResponseDTO> createPatient(@Valid @RequestBody PatientRequestDTO patientRequestDTO)
+    public ResponseEntity<PatientResponseDTO> createPatient(@Validated({Default.class, CreatePatientValidationGroup.class}) @RequestBody PatientRequestDTO patientRequestDTO)
     {
         PatientResponseDTO patientResponseDTO = patientService.createPatient(patientRequestDTO);
         return ResponseEntity.ok().body(patientResponseDTO);
@@ -40,7 +44,7 @@ public class PatientController {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<PatientResponseDTO> updatePatient(@PathVariable UUID id, @Valid @RequestBody PatientRequestDTO patientRequestDTO)
+    public ResponseEntity<PatientResponseDTO> updatePatient(@PathVariable UUID id, @Validated({Default.class}) @RequestBody PatientRequestDTO patientRequestDTO)
     {
         PatientResponseDTO patientResponseDTO = patientService.updatePatient(id,patientRequestDTO);
         return ResponseEntity.ok().body(patientResponseDTO);
